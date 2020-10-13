eded0771ffd1afa1cd852b53232cf81edd834cdf
samchrISU <samchr@iastate.edu>
2020-10-13 16:58:16 -0500

    Working Test Database

---
+++ webService/src/main/java/com/sisyphusWeb/webService/MainController.java
@@ -0,0 +1,29 @@
+package com.sisyphusWeb.webService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="/demo")
+public class MainController {
+	@Autowired
+	private UserRepository userRepository;
+	
+	@PostMapping(path="/add")
+	public @ResponseBody String addNewUser(@RequestParam String name) {
+		User n = new User();
+		n.setName(name);
+		userRepository.save(n);
+		return "Saved";
+	}
+	
+	@GetMapping(path="/all")
+	public @ResponseBody Iterable<User> getAllUsers() {
+		return userRepository.findAll();
+	}
+}
---
+++ webService/src/main/java/com/sisyphusWeb/webService/User.java
@@ -0,0 +1,32 @@
+package com.sisyphusWeb.webService;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+public class User {
+	@javax.persistence.Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer Id;
+	
+	private String name;
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
