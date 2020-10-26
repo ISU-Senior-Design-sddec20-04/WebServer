be69b0ac361a271cbddac39fcf1a0e2bb0d5370b
samchrISU <samchr@iastate.edu>
2020-10-26 04:29:30 -0500

    Download Works
    Get Request for image no longer returns 404

--- webService/src/main/java/com/sisyphusWeb/webService/WebServiceApplication.java
+++ webService/src/main/java/com/sisyphusWeb/webService/WebServiceApplication.java
@@ -3,7 +3,6 @@ package com.sisyphusWeb.webService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
 import com.sisyphusWeb.webService.property.FileStorageProperties;
 
 @SpringBootApplication
