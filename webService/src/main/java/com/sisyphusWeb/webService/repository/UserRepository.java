eae631fdcc0120c6e508fc93c5e2cbccee090149
Sgordon4 <SeanGordonkh@gmail.com>
2020-10-29 18:40:27 -0500

    Learning react branch

--- webService/src/main/java/com/sisyphusWeb/webService/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sisyphusWeb.webService.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.sisyphusWeb.webService.model.User;
-
-public interface UserRepository extends CrudRepository<User, Integer>{
-
-}
