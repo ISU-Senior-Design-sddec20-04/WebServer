27d3f689854de26a401a54ce33eec608904aa0d0
wjmaston <58753251+wjmaston@users.noreply.github.com>
2020-11-02 12:17:57 -0600

    Pictures for homepage

--- cysyphus_frontend/src/pages/Home.js
+++ cysyphus_frontend/src/pages/Home.js
@@ -1,69 +1,75 @@
 import React from 'react';
+import '../pages_styling/home.css'
 
 const Home = (props) => {
     return (
-        <div>
-                <div>
-                        <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Home Header</h1>
-                </div>
-            <img src={process.env.PUBLIC_URL + '/GroupMembers/Sam.jpg'}/>
-
-                <div id="twitch-embed"></div>
-                        <script src="https://embed.twitch.tv/embed/v1.js"></script>
-                <div>
-
-                </div>
-            <h1 style={{marginBottom: '4px', textAlign: 'center'}}>What is Cysyphus?</h1>
-            <p style={{marginBottom: '4px', textAlign: 'center'}}>Text for description here</p>
-
-                <div>
-                        <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Who are we?</h1>
-                </div>
 
 
-                <div>
-                        <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Samuel Christianson</h1>
 
-                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
-                </div>
-
-
-                <div>
-                        <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Morgan Funk</h1>
-                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Photo Here</p>
-                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
-                </div>
+        <body>
+            <div>
+                    <div>
+                            <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Home Header</h1>
+                    </div>
 
 
-                <div>
-                        <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Sean Gordon</h1>
-                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Photo Here</p>
-                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
-                </div>
+                    <div id="twitch-embed"></div>
+                            <script src="https://embed.twitch.tv/embed/v1.js"></script>
+                    <div>
+
+                    </div>
+                        <h1 style={{marginBottom: '4px', textAlign: 'center'}}>What is Cysyphus?</h1>
+                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Text for description here</p>
+
+                    <div className={'HomeWrapper'}>
+                            <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Who are we?</h1>
+                    </div>
 
 
-                <div>
-                        <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Daniel Laracuenta</h1>
-                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Photo Here</p>
-                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
-                </div>
+                    <div className={'HomeWrapper'}>
+                            <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Samuel Christianson</h1>
+                            <img src={process.env.PUBLIC_URL + '/Assets/GroupMembers/Sam.jpg'} alt={"Team Member"}/>
+                            <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
+                    </div>
 
 
-                <div>
-                        <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Aaron Lawrence</h1>
-                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Photo Here</p>
-                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
-                </div>
+                    <div className={'HomeWrapper'}>
+                            <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Morgan Funk</h1>
+                            <img src={process.env.PUBLIC_URL + '/Assets/GroupMembers/Morgan.jpg'} alt={"Team Member"}/>
+                            <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
+                    </div>
 
 
-                <div>
-                        <h1 style={{marginBottom: '4px', textAlign: 'center'}}>William Maston</h1>
-                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Photo Here</p>
-                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
-                </div>
+                    <div className={'HomeWrapper'}>
+                            <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Sean Gordon</h1>
+                            <img src={process.env.PUBLIC_URL + '/Assets/GroupMembers/Sean2.jpg'} alt={"Team Member"}/>
+                            <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
+                    </div>
 
 
-        </div>
+                    <div className={'HomeWrapper'}>
+                            <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Daniel Laracuenta</h1>
+                            <img src={process.env.PUBLIC_URL + '/Assets/GroupMembers/Daniel2.jpg'} alt={"Team Member"}/>
+                            <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
+                    </div>
+
+
+                    <div className={'HomeWrapper'}>
+                            <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Aaron Lawrence</h1>
+                            <img src={process.env.PUBLIC_URL + '/Assets/GroupMembers/Aaron.jpg'} alt={"Team Member"}/>
+                            <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
+                    </div>
+
+
+                    <div className={'HomeWrapper'}>
+                            <h1 style={{marginBottom: '4px', textAlign: 'center'}}>William Maston</h1>
+                            <img src={process.env.PUBLIC_URL + '/Assets/GroupMembers/Will.jpg'} alt={"Team Member"}/>
+                            <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
+                    </div>
+
+
+            </div>
+        </body>
     );
 }
 
