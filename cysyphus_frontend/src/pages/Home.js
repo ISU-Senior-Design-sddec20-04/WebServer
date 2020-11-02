3334cae1db3e35666edd8756c8cb60a0e12fc384
wjmaston <58753251+wjmaston@users.noreply.github.com>
2020-11-02 03:21:31 -0600

    Home Page Layout, and messing around with twitch

--- cysyphus_frontend/src/pages/Home.js
+++ cysyphus_frontend/src/pages/Home.js
@@ -1,10 +1,67 @@
 import React from 'react';
 
-const Home = () => {
+const Home = (props) => {
     return (
         <div>
-            <h1>Home</h1>
-            <p>Sample about us</p>
+                <div>
+                        <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Home Header</h1>
+                </div>
+
+                <div id="twitch-embed"></div>
+                        <script src="https://embed.twitch.tv/embed/v1.js"></script>
+                <div>
+
+                </div>
+            <h1 style={{marginBottom: '4px', textAlign: 'center'}}>What is Cysyphus?</h1>
+            <p style={{marginBottom: '4px', textAlign: 'center'}}>Text for description here</p>
+
+                <div>
+                        <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Who are we?</h1>
+                </div>
+
+
+                <div>
+                        <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Samuel Christianson</h1>
+                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Photo Here</p>
+                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
+                </div>
+
+
+                <div>
+                        <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Morgan Funk</h1>
+                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Photo Here</p>
+                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
+                </div>
+
+
+                <div>
+                        <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Sean Gordon</h1>
+                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Photo Here</p>
+                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
+                </div>
+
+
+                <div>
+                        <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Daniel Laracuenta</h1>
+                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Photo Here</p>
+                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
+                </div>
+
+
+                <div>
+                        <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Aaron Lawrence</h1>
+                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Photo Here</p>
+                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
+                </div>
+
+
+                <div>
+                        <h1 style={{marginBottom: '4px', textAlign: 'center'}}>William Maston</h1>
+                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Photo Here</p>
+                        <p style={{marginBottom: '4px', textAlign: 'center'}}>Text here</p>
+                </div>
+
+
         </div>
     );
 }
