e9d6a781926d3d4c44f386a8fbc46f8e432aa26e
wjmaston <58753251+wjmaston@users.noreply.github.com>
2020-11-05 13:48:41 -0600

    Stole some login features for the password change

--- cysyphus_frontend/src/pages/Settings.js
+++ cysyphus_frontend/src/pages/Settings.js
@@ -1,28 +1,55 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import "../widgets_styling/Login.css";
 import '../pages_styling/home.css'
 
 
 
 const Settings = (props) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
     return (
-
-
-
-        <body>
-            <div>
-                <div className={'HomeWrapper'}>
-                    <h1 style={{marginBottom: '4px', textAlign: 'center'}}>Settings</h1>
-                </div>
-
-
-
-
-
-
-
-
-            </div>
-        </body>
+        <div className="Login">
+            <form onSubmit={handleSubmit}>
+                <FormGroup controlId="email" bsSize="large">
+                    <FormLabel>Email</FormLabel>
+                    <FormControl
+                        autoFocus
+                        type="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                </FormGroup>
+                <FormGroup controlId="password" bsSize="large">
+                    <FormLabel>Old Password</FormLabel>
+                    <FormControl
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        type="password"
+                    />
+                </FormGroup>
+                <FormGroup controlId="password" bsSize="large">
+                    <FormLabel>New Password</FormLabel>
+                    <FormControl
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        type="password"
+                    />
+                </FormGroup>
+                <Button block bsSize="large" disabled={!validateForm()} type="submit">
+                    Change
+                </Button>
+            </form>
+        </div>
     );
 }
 
